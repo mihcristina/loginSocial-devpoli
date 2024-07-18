9ce8280fe09c90858df590ed2715f07682fe50d7
Michelli Cristina de Paulo Lima <michellicristinadepaulolima@MacBook-Air-de-Michelli.local>
2024-07-17 22:20:09 -0300

    cria viewmodel

---
+++ loginSocial-devpoli/Login/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  loginSocial-devpoli
+//
+//  Created by Michelli Cristina de Paulo Lima on 17/07/24.
+//
+
+import UIKit
+
+protocol LoginViewModelDelegate: AnyObject {
+    
+}
+
+class LoginViewModel: LoginViewModelDelegate {
+    
+}
